cfa5a69348fbbd656c57748b3c2a6c8fb202f73f
Nick Ragaz <nick@wellx.ca>
2015-01-06 23:41:40 -0500

    Fix middleware insertion

--- lib/font_assets/railtie.rb
+++ lib/font_assets/railtie.rb
@@ -8,13 +8,11 @@ module FontAssets
       config.font_assets.origin ||= "*"
       config.font_assets.options ||= { allow_ssl: true }
 
-      insert_target = if defined?(ActionDispatch::Static)
-        'ActionDispatch::Static'
-      else
-        'Rack::Runtime'
+      begin
+        app.middleware.insert_before 'ActionDispatch::Static', FontAssets::Middleware, config.font_assets.origin, config.font_assets.options
+      rescue
+        app.middleware.insert_before 'Rack::Runtime', FontAssets::Middleware, config.font_assets.origin, config.font_assets.options
       end
-
-      app.middleware.insert_before insert_target, FontAssets::Middleware, config.font_assets.origin, config.font_assets.options
     end
   end
 end
